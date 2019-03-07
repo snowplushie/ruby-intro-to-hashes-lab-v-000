93521fdde2c1699fe92722f5011dafb22d81e3f1
Paige Wiener <snowplushie@yahoo.com>
2019-03-07 04:48:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,8 +24,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+	
 end
 
 def monopoly_with_fourth_tier
